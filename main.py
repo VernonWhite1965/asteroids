1b26cc300faab7152bdbb37dbb9fb8a4c92bb392
VernonWhite1965 <vernon_white@hotmail.com>
2025-01-01 13:49:05 -0600

    008: put player in to group containers

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
 
 
 def main():
-	# Init pygame and set screen parameter to width and height from constants.txt
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-	# start pygame clock and set delta time (dt) to zero
-	clock = pygame.time.Clock()
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-	dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-	# print starting game and screen size
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+    Player.containers = (updatable, drawable)
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-	# infinite loop to start the game screen with abilty to quit
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
+    dt = 0
 
-		player.update(dt)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
-		screen.fill("black")
-		player.draw(screen)
-		pygame.display.flip()
+        for obj in updatable:
+            obj.update(dt)
 
-		# limit framerate to 60 fps
-		dt = clock.tick(60) / 1000
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-	main()
+    main()
