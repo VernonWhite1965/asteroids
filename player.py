412d502e5dc83a6a29d0674b91e744ed65e5009a
VernonWhite1965 <vernon_white@hotmail.com>
2025-01-01 11:36:53 -0600

    005: update to rotate commands after checking program

--- player.py
+++ player.py
@@ -14,7 +14,7 @@ class Player(CircleShape):
 
 	# draw function to convert the circle boundary to triangle shape
 	def draw(self, screen):
-	pygame.draw.polygon(screen, "white", self.triangle(), 2)
+		pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
 	# in the player class
 	def triangle(self):
@@ -26,7 +26,7 @@ class Player(CircleShape):
 		return [a, b, c]
 
 	# rotate player
-	def rotate(self):
+	def rotate(self, dt):
 		self.rotation += PLAYER_TURN_SPEED * dt
 
 
