06565c15250ff4f5240b67d5cf891b57f579e981
VernonWhite1965 <vernon_white@hotmail.com>
2025-01-01 10:41:35 -0600

    001: Update main with clock.tick to 60 fps

--- main.py
+++ main.py
@@ -4,40 +4,32 @@
 import pygame
 from constants import *
 
-'''
-def start_pygame(self):
-	pygame.init()
-	""" Initialize pygame system stuff and draw empty window """
-	if not pygame.display.get_init():
-		pygame.display.init()
-	if not pygame.font.get_init():
-		pygame.font.init()
-
-def game_loop(self):
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-		screen = pygame.display.set_mode((SCEEN_WIDHR, SCREEN_HEIGHT))
-		screen.fill("black")
-		pygame.display.flip()
-
-'''
 
 def main():
+	# Init pygame and set screen parameter to width and height from constants.txt
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	# start pygame clock and set delta time (dt) to zero
+	clock = pygame.time.Clock()
+	dt = 0
+
+	# print starting game and screen size
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-	
+
+
+	# infinite loop to start the game screen with abilty to quit
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
 		pygame.display.flip()
-
+		
+		# limit framerate to 60 fps
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
